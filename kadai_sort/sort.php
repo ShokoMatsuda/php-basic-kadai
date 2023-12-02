7fabfa24981649857fa07b494096fccf11ed5429
ShokoMatsuda <142991303+ShokoMatsuda@users.noreply.github.com>
2023-12-02 21:10:07 +0900

    タイトルとorder引数のデータ型を修正

--- kadai_sort/sort.php
+++ kadai_sort/sort.php
@@ -2,14 +2,14 @@
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
-    <title>PHP基礎編<</title>
+    <title>PHP基礎編</title>
 </head>
 <body>
     <p>
         <?php
 
         function sort_2way($array, string $order) {
-            if ($order === '昇順') {
+            if ($order == TRUE) {
                 echo '昇順にソートします<br>';
                 sort($array);
                 foreach ($array as $arrays) {
@@ -28,9 +28,9 @@
         // 配列
         $array = [15, 4, 18, 23, 10];
 
-        sort_2way($array, '昇順');
+        sort_2way($array, TRUE);
 
-        sort_2way($array, '降順');
+        sort_2way($array, FALSE);
         ?>
     </p>
 </body>
